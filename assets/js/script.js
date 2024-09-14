4ee2feb5446cc62aa6e9dddb38152e992832d87d
Gustavo Matias <gustavomstimao@gmail.com>
2024-09-14 03:04:15 -0300

    commit-bugs-in-services

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
@@ -13,7 +14,7 @@ window.onscroll = () => {
     if(top >= offset && top < offset + height){
       navLinks.forEach(links => {
         links.classList.remove('active');
-        //document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+        // document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
       })
     }
   })
@@ -32,3 +33,9 @@ function mostrarTela(id) {
   // Mostra a tela selecionada
   document.getElementById(id).style.display = 'block';
 }
+
+//alert from thanks
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  alert("Thanks for contact");
+});
